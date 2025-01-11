b31c5a5f47cddc15fe4ee289970d6227a5d77cf9
Ivan Levandy <ub1vashkaFROMrussia@gmail.com>
2025-01-11 13:36:05 +0300

    main

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/ub1vashka/golangfirstproject/internal/config"
+	"github.com/ub1vashka/golangfirstproject/internal/logger"
+	"github.com/ub1vashka/golangfirstproject/internal/server"
+	"github.com/ub1vashka/golangfirstproject/internal/service"
+	"github.com/ub1vashka/golangfirstproject/internal/storage"
 )
 
 func main() {
-	cfg := config.ReadConfig
+	cfg := config.ReadConfig()
+	log := logger.Get(cfg.Debug)
 
-	fmt.Println(cfg)
+	stor := storage.New()
+	userService := service.NewUserService(stor)
+	bookService := service.NewBookService(stor)
+	serve := server.New(cfg, userService, bookService)
+	if err := serve.Run(); err != nil {
+		log.Fatal().Err(err).Send()
+	}
 }
