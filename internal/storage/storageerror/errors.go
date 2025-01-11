b514f00e77488f2669892d6b9dc74f10c6bc33a9
Ivan Levandy <ub1vashkaFROMrussia@gmail.com>
2025-01-11 11:23:46 +0300

    errors

--- internal/storage/storageerror/errors.go
+++ internal/storage/storageerror/errors.go
@@ -3,7 +3,9 @@ package storageerror
 import "errors"
 
 var (
-	ErrBookAlredyExist = errors.New("book alredy exist")
-	ErrEmptyStorage    = errors.New("book storage is empty")
-	ErrBookNoFound     = errors.New("book not found")
+	ErrBookAlredyExist  = errors.New("book alredy exist")
+	ErrBookByIdNotFound = errors.New("book id not found")
+	ErrEmptyStorage     = errors.New("book storage is empty")
+	ErrBookNoFound      = errors.New("book not found")
+	ErrBookIDNotFound   = errors.New("book id not found")
 )
